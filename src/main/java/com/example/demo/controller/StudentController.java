2b73cecb2276fa7ae0babc8be4a8fdc78308ccdb
KingsleyLZJ <2810685251@qq.com>
2019-01-14 11:20:52 +0800

    commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//gyufyfyu
 @RestController
 @RequestMapping("stu")
 public class StudentController {
